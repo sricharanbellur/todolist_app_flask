faf7184812a2a7ecfa45a4eff52d5a63e1f6c3a4
sricharanbellur <84399688+sricharanbellur@users.noreply.github.com>
2022-08-26 13:00:13 +0530

    Update index.py

--- index.py
+++ index.py
@@ -3,8 +3,18 @@ app = Flask(__name__)
 @app.route('/register')
 def register():
     return render_template('register.html')
+@app.route('/save',methods=['POST'])
+def save():
+    return render_template('succesfully.html',display=request.form)
+
 @app.route('/login')
 def login():
     return render_template('login.html')
-
+def auth():
+    response=dict(request.form)
+    if(response['usertxt']=='admin' and response['passtxt']=='admin123'):
+        return render_template('todo.html',response=response)
+    else:
+        return render_template('error.html')
+ app.run()
     
